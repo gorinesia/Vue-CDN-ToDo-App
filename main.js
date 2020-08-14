d87a22d472c4ea5c284fdaa0367a983437305608
Akinori Sakakibara <gorion.1213@gmail.com>
2020-08-14 11:22:15 +0900

    initial commit for cdn vue.js

---
+++ main.js
@@ -0,0 +1,15 @@
+// new Vue({
+//   el: '#app',
+//   data() {
+//     return {
+//       text1: '',
+//       text2: '',
+//     }
+//   },
+//   watch: {
+//     text1(newValue, oldValue) {
+//       console.log('new', newValue);
+//       console.log('old', oldValue);
+//     }
+//   }
+// })
